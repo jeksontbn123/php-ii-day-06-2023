36060ffa0ac9fc8b69c7a3e395691da977a5dd30
Jekson <100131109+jeksontbn123@users.noreply.github.com>
2023-04-11 18:12:50 +0700

    update belajar

--- include/header.php
+++ include/header.php
@@ -1,6 +1,4 @@
-<!-- HEADER START -->
 <header class="border border-gray-600 container mx-auto p-6">
-        <h1>SaCode News</h1>
+        <h1 class="font-bold text-6xl mb-3">SaCode News</h1>
         <p>Berbagai berita terbaru seputar SaCode</p>
     </header>
-    <!-- HEADER END -->
--- include/nav.php
+++ include/nav.php
@@ -1,11 +1,9 @@
- <!-- NAV START -->
- <nav class="border border-gray-600 container mx-auto p-6">
-        <a href="?page=beranda" class="border py-3 px-4 bg-gray-700 hover">Beranda</a> 
-        <a href="?category=teknologi" class="border py-3 px-4 hover">Teknologi</a>
-        <a href="?category=pendidikan" class="border py-3 px-4  hover">Pendidikan</a>
-        <a href="?category=pengembangan diri"class="border py-3 px-4  hover">Pengembangan Diri</a>
-        <a href="?category=kurus"class="border py-3 px-4  hover">Kursus</a>
-        <a href="?category=komunitas"class="border py-3 px-4  hover">Komunitas</a>
-        <a href="?category=lainnya"class="border py-3 px-4  hover">Lainnya</a>
+<nav class="border border-gray-600 container mx-auto p-6">
+        <a href="?page=beranda" class="border py-3 px-4 bg-gray-700 hover:bg-gray-600 transition text-gray-100">Beranda</a> 
+        <a href="?category=teknologi" class="border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition">Teknologi</a>
+        <a href="?category=pendidikan" class="border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition">Pendidikan</a>
+        <a href="?category=pengembangan-diri" class="border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition">Pengembangan Diri</a>
+        <a href="?category=kursus" class="border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition">Kursus</a>
+        <a href="?category=komunitas" class="border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition">Komunitas</a>
+        <a href="?category=lainnya" class="border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition">Lainnya</a>
     </nav>
-    <!-- NAV END -->
